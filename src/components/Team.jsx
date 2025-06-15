cfa5ec1f349cce16aa2cf88d9006a90e400e5f27
blackkite88 <vivek124680@gmail.com>
2025-06-15 19:48:07 +0530

    updates

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import mem_image from '../assets/dummyteam.jpg'
 import TeamCard from './TeamCard'
+import logo from '../assets/dummycard.png'
+import { Carousel } from "flowbite-react";
+import EventCard from './EventCard.jsx'
+
+const slides=[
+  mem_image,
+  mem_image,
+  mem_image,
+  mem_image,
+  mem_image,
+  mem_image
+]
 
 const Team = () => {
   return (
-    <div className='flex flex-col items-center text-center'>
-      <h1 className="text-white text-3xl sm:text-5xl my-12 font-bold">Our Team</h1>
-      <div className="z-[-1] flex flex-wrap backdrop-blur-xl backdrop-filter mx-6 bg-white/20 bg-opacity-10 justify-evenly p-1">
-        <TeamCard name={`Vivek`} role={`Technical Team Lead`} image={mem_image}/>
-        <TeamCard name={`Vivek`} role={`Technical Team Lead`} image={mem_image}/>
-        <TeamCard name={`Vivek`} role={`Technical Team Lead`} image={mem_image}/>
-        <TeamCard name={`Vivek`} role={`Technical Team Lead`} image={mem_image}/>
-        <TeamCard name={`Vivek`} role={`Technical Team Lead`} image={mem_image}/>
-        <TeamCard name={`Vivek`} role={`Technical Team Lead`} image={mem_image}/>
-        <TeamCard name={`Vivek`} role={`Technical Team Lead`} image={mem_image}/>
-        <TeamCard name={`Vivek`} role={`Technical Team Lead`} image={mem_image}/>
-        <TeamCard name={`Vivek`} role={`Technical Team Lead`} image={mem_image}/>
-        <TeamCard name={`Vivek`} role={`Technical Team Lead`} image={mem_image}/>
-        <TeamCard name={`Vivek`} role={`Technical Team Lead`} image={mem_image}/>
-        <TeamCard name={`Vivek`} role={`Technical Team Lead`} image={mem_image}/>
-        <TeamCard name={`Vivek`} role={`Technical Team Lead`} image={mem_image}/>
-
+    <div className="flex flex-col relative items-center">
+      <h1 className="text-white relative text-3xl sm:text-5xl my-12 font-bold">Our Team</h1>
+      <div className="w-3/4 relative backdrop-blur-xl backdrop-filter mx-6 mb-10 bg-white/20 bg-opacity-10 p-2">
+        <Carousel indicators={false}>
+          {slides.map((s) => 
+          <img className='relative' src={s} alt="" />
+          )}
+        </Carousel>
       </div>
     </div>
   )
--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TeamCard = ({name, role, image}) => {
+const TeamCard = ({image}) => {
   return (
     <div id='event-card' className='h-55 w-40 lg:h-90 lg:w-70 flex flex-col justify-between bg-amber-50 m-3 md:m-4'>
       <img className='h-[70%] ' src={image} alt="" />
