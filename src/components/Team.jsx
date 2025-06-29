6733961c26b65dc3dd3f7b87ce7c204c8c2e0abf
blackkite <vivek124680@gmail.com>
2025-06-29 22:21:28 +0530

    team update

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -5,6 +5,8 @@ import logo from '../assets/dummycard.png'
 import { Carousel } from "flowbite-react";
 import EventCard from './EventCard.jsx'
 import img1 from'../assets/image.png'
+import { useState } from 'react';
+
 const slides=[
   mem_image,
   mem_image,
@@ -15,17 +17,29 @@ const slides=[
 ]
 
 const Team = () => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
+    <div className='relative'>
+    {isHovered && <div className="pointer-events-none
+ z-[101] fixed inset-0 h-screen w-screen bg-black/80"></div>}
     <div id='xx1' className="flex flex-col relative text-center justify-center items-center m-5">
       <div className="flex flex-col m-5 relative text-center justify-center items-center">
-        <h1 className='font-bold text-[#c6c1c1] w-78 sm:w-120 md:w-150 lg:w-150 text-lg sm:text-xl md:text-4xl' >FACULTY ADVISOR</h1>
+        <h1 className='font-bold text-[#c6c1c1] text-lg sm:text-xl md:text-4xl' >FACULTY ADVISOR</h1>
         <div className="w-44 h-1 bg-gradient-to-r from-red-600 to-red-400 mx-auto mt-2 mb-5 rounded-full"></div>
-        <img className='h-[250px] w-[200px] lg:h-[450px] lg:w-[400px]' src={img1} alt="" />
+        <TeamCard />
+
+        {/* <img className='h-[250px] w-[200px] lg:h-[450px] lg:w-[400px]' src={img1} alt="" /> */}
       </div>
       <div className="w-screen flex m-5 flex-col relative text-center justify-center items-center">
-        <h1 className='font-bold text-[#c6c1c1] w-78 sm:w-120 md:w-150 lg:w-150 text-lg sm:text-xl md:text-4xl' >EXECUTIVE BODY</h1>
+        <h1 className='font-bold text-[#c6c1c1] text-lg sm:text-xl md:text-4xl' >EXECUTIVE BODY</h1>
         <div className="w-44 h-1 bg-gradient-to-r from-red-600 to-red-400 mx-auto mt-2 mb-5 rounded-full"></div>
-        <div className="flex flex-wrap justify-center gap-2 mx-2 md:gap-4">
+        <div className="flex flex-wrap justify-center gap-2 mx-2 md:gap-4 ">
+          <TeamCard />
+          <TeamCard />
+          <TeamCard />
+          <TeamCard />
+          <TeamCard />
+          <TeamCard />
           <img className='h-[200px] w-[135px] lg:h-[350px] lg:w-[300px]' src={img1} alt="" />
           <img className='h-[200px] w-[135px] lg:h-[350px] lg:w-[300px]' src={img1} alt="" />
           <img className='h-[200px] w-[135px] lg:h-[350px] lg:w-[300px]' src={img1} alt="" />
@@ -73,6 +87,7 @@ const Team = () => {
           <img className='h-[200px] w-[135px] lg:h-[350px] lg:w-[300px]' src={img1} alt="" />
         </div>
       </div>
+    </div>
     </div>
   )
 }
--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import img from '../assets/image.png';
 
-const TeamCard = ({image}) => {
+const TeamCard = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div id='event-card' className='h-55 w-40 lg:h-90 lg:w-70 flex flex-col justify-between bg-amber-50 m-3 md:m-4'>
-      <img className='h-[70%] ' src={image} alt="" />
-      <h2 className='text-3xl font-bold '>{name}</h2>
-      <p className='text-lg mx-5 mb-5'>{role}</p>
-    </div>
-  )
-}
+    <div
+      className="relative"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className="bg-white overflow-hidden flex flex-col h-[200px] w-[135px] lg:h-[350px] rounded-2xl lg:w-[300px]">
+        <img
+          className="rounded-2xl h-[200px] w-[135px] lg:h-[350px] lg:w-[300px]"
+          src={img}
+          alt=""
+        />
+      </div>
 
-export default TeamCard
+      {isHovered && (
+        <>
+          <div className="fixed inset-0 bg-black/40 backdrop-blur-sm z-[1000] pointer-events-none"></div>
+
+          <div id='cardx' className="fixed top-1/2 left-1/2 rounded-2xl z-[1001] transform -translate-x-1/2 -translate-y-1/2 bg-white flex flex-row shadow-2xl transition-all duration-300 ease-in-out w-[310px] md:w-[470px] lg:w-[600px] h-[350px] scale:110 md:scale-150">
+            <img
+              className="rounded-2xl h-full w-[150px] md:w-[210px] lg:w-[300px]"
+              src={img}
+              alt=""
+            />
+            <div className="flex flex-col text-left justify-center px-5">
+              <h4 className="font-bold text-[#ff0000] text-xl md:text-2xl lg:text-4xl mb-2">
+                Archana Mallick
+              </h4>
+              <p className="font-semibold text-[#c37f7f] text-lg md:text-xl lg:text-3xl mb-7">
+                Faculty Advisor
+              </p>
+              <p className="font-medium text-[#c6c1c1] text-md md:text-lg lg:text-2xl mb-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam,
+                expedita!
+              </p>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default TeamCard;
